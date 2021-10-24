5d5af245ed2030772a1e2b8bf9d9c1be534581ef
LovePlanes <33170989+GeetanshGautam0@users.noreply.github.com>
2021-10-24 16:16:06 -0700

    Added local theme checks in diagnostics script

--- diagnostics.py
+++ diagnostics.py
@@ -1,5 +1,5 @@
 import os, conf, theme, json, re
-
+import traceback
 
 FALSE = 0
 TRUE = 1
@@ -258,7 +258,7 @@ class DataDiagnostics:
                 },
                 'font': {
                     "information": "Font information",
-                    "type": str,
+                    "type": dict,
                     'required': TRUE,
                     'children': {
                         'font_face': {
@@ -293,7 +293,8 @@ class DataDiagnostics:
                             "information": "The smallest size used in the application (most common)",
                             "type": int,
                             'required': TRUE,
-                            'default': 10
+                            'default': 10,
+                            'children': {}
                         }
                     }
                 },
@@ -327,12 +328,24 @@ class DataDiagnostics:
             }
 
         @staticmethod
-        def check_defaults():
+        def check_defaults() -> tuple:
             failed = passed = violations = ()
             for item in os.listdir(conf.ConfigFile.raw['theme']['theme_root']):
+                item = os.path.join(
+                    conf.ConfigFile.raw['theme']['theme_root'],
+                    item
+                )
+
                 if os.path.isdir(item):
-                    for file in item:
-                        if os.path.isfile(item):
+                    for file in os.listdir(item):
+                        print(item)
+
+                        file = os.path.join(
+                            item,
+                            file
+                        )
+
+                        if os.path.isfile(file):
                             if file.split('.')[-1] in ['png', 'json']:
                                 if file.split('.')[-1] == 'json':
                                     with open(os.path.abspath(file), 'r') as jfile:
@@ -340,10 +353,10 @@ class DataDiagnostics:
                                         jfile.close()
                                     try:
                                         r = json.loads(r)
-                                        res = _check_theme_file(r)
+                                        res = _check_theme_file(r, file.split("\\")[-1])
                                         failed = (*failed, *res['failed'])
-                                        passed = (*failed, *res['passed'])
-                                        violations = (*failed, *res['violations'])
+                                        passed = (*passed, *res['passed'])
+                                        violations = (*violations, *res['violations'])
 
                                     except:
                                         failed = (*failed, "FAILURE: Failed to load theme file %s" % os.path.abspath(file))
@@ -365,6 +378,9 @@ class DataDiagnostics:
                 else:
                     violations = (*violations, 'VIOLATION: Non-dir item found in theme_root directory (%s)' % item)
 
+            output = (passed, failed, violations)
+            return output
+
         @staticmethod
         def check_user_pref():
             pass
@@ -382,44 +398,92 @@ def _run_file_exs_check(req: tuple):
     return output
 
 
-def _deduce_theme_data_result(data) -> dict:
-    pass
+def _deduce_theme_data_result(raw, req, cond_checks, file_path, key_dir='root') -> dict:
+    output = {
+        'failed': (),
+        'passed': (),
+        'violations': ()
+    }
+
+    key_directory = file_path.replace("\\", "//") + "/" + key_dir
+
+    for key, requirement in req.items():
+        if key not in raw and requirement['required'] == TRUE:
+            output['failed'] = (
+                *output['failed'],
+                "FAILURE: Key '%s' does not exist in theme file (%s/%s)." % (key, key_directory, key)
+            )
+
+        else:
+            output['passed'] = (*output['passed'],
+                                "PASSED: Found key '%s/%s' in theme file." % (key_directory, key))
+
+            if 'conditions' in requirement:
+                for condition in requirement['conditions']:
+                    if condition != "len[d, l] > 0":
+                        result = True
+                        for check in cond_checks[condition]:
+                            result = result and check(raw[key])
+
+                        if result:
+                            output['passed'] = (
+                                *output['passed'],
+                                "PASSED: key '%s/%s' passed condition '%s'" % (key_directory, key, condition)
+                            )
+                        else:
+                            output['failed'] = (
+                                *output['failed'],
+                                "FAILURE: key '%s/%s' failed condition '%s'" % (key_directory, key, condition)
+                            )
+
+                    else:
+                        if 'light' in raw or 'dark' in raw:
+                            output['passed'] = (
+                                *output['passed'],
+                                "PASSED: key '%s/%s' passed condition '%s'" % (key_directory, key, condition)
+                            )
+
+            if type(raw[key]) is requirement['type']:
+                output['passed'] = (
+                    *output['passed'],
+                    "PASSED: key '%s/%s' is type '%s'" % (key_directory, key, str(requirement['type']))
+                )
+
+            else:
+                output['failed'] = (
+                    *output['failed'],
+                    "FAILURE: key '%s/%s' is type '%s', but expected type '%s'" % (key_directory, key, str(type(raw[key])), str(requirement['type']))
+                )
+
+            for _ in requirement['children']:
+                res = _deduce_theme_data_result(raw[key], req[key]['children'], cond_checks, "", key_directory + "/" + key)
+                output['failed'] = (*output['failed'], *res['failed'])
+                output['passed'] = (*output['passed'], *res['passed'])
+                output['violations'] = (*output['violations'], *res['violations'])
+
+    for key, items in output.items():
+        output[key] = (*set(items), )
+
+    return output
 
 
-def _check_theme_file(raw) -> dict:
+def _check_theme_file(raw, file_path) -> dict:
     try:
-        output = {
-            'failed': (),
-            'passed': (),
-            'violations': ()
-        }
 
         req = DataDiagnostics.Theme._FileData.data_type
         cond_checks = DataDiagnostics.Theme._FileData.conditional_checks
 
-        for key, requirement in req.items():
-            if key not in raw:
-                output['failed'] = (
-                    *output['failed'],
-                    "FAILURE: Key '%s' does not exist in theme file (root/%s)." % (key, key)
-                )
+        result = _deduce_theme_data_result(raw, req, cond_checks, file_path)
+        output = result
 
-            else:
-                output['passed'] = (*output['passed'],
-                                    "PASS: Found key 'root/%s' in theme file." % key)
-
-                if 'conditions' in requirement:
-                    for condition in requirement['conditions']:
-                        if condition != "len[d, l] > 0":
-                            result = cond_checks[condition](raw[key])
-
-                if 'children' in requirement:
-                    pass
+        return output
 
     except Exception as E:
+        print(traceback.format_exc())
+
         return {
             'failed': (
-                'FAILURE: Failed to check file: %s.' % E.__str__()
+                'FAILURE: Failed to check file: %s.' % E.__str__(),
             ),
             'passed': (),
             'violations': ()
